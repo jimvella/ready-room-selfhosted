243a9e9c6a873fe3704d137ab5c7d93c21d7e43f
frank99123 <113268838+frank99123@users.noreply.github.com>
2026-03-22 10:11:54 -0400

    Update startup.ps1 to include WebGUI execution
    Include WebGui execution so that user can alt-tab into DCS server WebGui in case they need to RDP and make any changes.

--- startup.ps1
+++ startup.ps1
@@ -223,6 +223,7 @@ New-Item -Path "C:\Users\Administrator\Saved Games\DCS.dcs_serverrelease" -Name 
 echo $ServerSettings | Out-File -Encoding ascii -Filepath "C:\Users\Administrator\Saved Games\DCS.dcs_serverrelease\Config\serverSettings.lua"
 
 cd "Z:\DCS World Server"
+WebGUI\index.html
 bin\DCS_server.exe
 
 (Poll { ((Get-Process DCS_Server).Id | ForEach-Object { [FlaUI.Core.Application]::Attach($_) } | ForEach-Object { $_.GetAllTopLevelWindows( $automation ) } | ForEach-Object { $_.SetForeground(); $_ } | ForEach-Object { Nested-Children($_) } | Where-Object { $_.ControlType -eq "Edit"}) } { param($i) $i.Count -ne 0 } $interval $timeout $delay $reportingInterval)[0].Patterns.Value.Pattern.SetValue($serverUsername)
